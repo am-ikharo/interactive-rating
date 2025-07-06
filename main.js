5fb71b2ff1159db2159bd86acb01584164e97395
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-07-06 12:41:03 +0100

    trying to catch the selected rating value

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ const getRating = document.querySelectorAll('.rating');
 const getSelectedRatingValue = document.querySelector('.selected-rating-value');
 
 let currentTotalSelectedRating = -1;
+
+getRating.forEach((currentItem, index) => {
+    currentItem.dataset.rating = index +1;
+    currentItem.addEventListener('click', (event) => {
+        const currentRatingValue = event.target.dataset
+        getSelectedRatingValue = currentRatingValue
+        
+    })
+})
